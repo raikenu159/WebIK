89c8c35c05221f8638e8bc872a336d474c85e0d0
Sem <semkjaer@hotmail.nl>
2020-01-14 19:56:15 +0000

    puntjes op de i

--- test.py
+++ test.py
@@ -14,24 +14,18 @@ print()
 # de vragen
 questions = response['results']
 # zo zien die er uit
+i=1
 for question in questions:
-    print(question)
+    print(i, question)
     print()
+    i+= 1
 
 # html.unescape() maakt text van gecodeerde leestekens zoals die voorkomen in sommige vragen
 print("voor: &quot;The Sims&quot;?")
 print("na: " + html.unescape("&quot;The Sims&quot;?"))
 print()
 
-# categorieen
-categorieen = [{'id': 9, 'name': 'General Knowledge'}, {'id': 10, 'name': 'Entertainment: Books'},
-{'id': 11, 'name': 'Entertainment: Film'}, {'id': 12, 'name': 'Entertainment: Music'}, {'id': 13, 'name': 'Entertainment: Musicals & Theatres'},
-{'id': 14, 'name': 'Entertainment: Television'}, {'id': 15, 'name': 'Entertainment: Video Games'}, {'id': 16, 'name': 'Entertainment: Board Games'},
-{'id': 17, 'name': 'Science & Nature'}, {'id': 18, 'name': 'Science: Computers'}, {'id': 19, 'name': 'Science: Mathematics'},
-{'id': 20, 'name': 'Mythology'}, {'id': 21, 'name': 'Sports'}, {'id': 22, 'name': 'Geography'}, {'id': 23, 'name': 'History'},
-{'id': 24, 'name': 'Politics'}, {'id': 25, 'name': 'Art'}, {'id': 26, 'name': 'Celebrities'}, {'id': 27, 'name': 'Animals'},
-{'id': 28, 'name': 'Vehicles'}, {'id': 29, 'name': 'Entertainment: Comics'}, {'id': 30, 'name': 'Science: Gadgets'},
-{'id': 31, 'name': 'Entertainment: Japanese Anime & Manga'}, {'id': 32, 'name': 'Entertainment: Cartoon & Animations'}]
+categorieen = requests.get("https://opentdb.com/api_category.php").json()['trivia_categories'] # hierin staan ook de catagory IDs als je die nog wilt zien
 
 # er waren best veel categorieen met tussen de 45 en 50 vragen en t verschil lijkt me niet relevant dus ik heb een lijstje gemaakt van categorieen
 # waar meer dan 45 vragen in zitten, ik denk dat we uit deze moeten kiezen
