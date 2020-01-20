530368fa87a93f2278536a92ce6ca001d7e327c9
Sem <semkjaer@hotmail.nl>
2020-01-20 22:16:47 +0000

    dinguh

--- application.py
+++ application.py
@@ -1,18 +1,21 @@
 import os
 import uuid
-
+import requests
 from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 from flask_session import Session
+from tempfile import mkdtemp
 from datetime import datetime
-
-
 
 # Configure application
 app = Flask(__name__)
 
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 # Ensure responses aren't cached
 @app.after_request
@@ -45,29 +48,10 @@ def quiz():
     """Take quiz"""
 
     if request.method == "POST":
-        print("POST request")
-    #     # nadat de check functie is uitgevoerd worden de username(als de gebruiker in de top 10 zit) en de score gesubmit
-    #     # deze functie voegt deze dan toe aan de database zodat deze op de leaderboard weergegeven kunnen worden
-    #     id = session["user_id"]
-    #     username = request.form.get('username')
-    #     scoreTotaal = request.form.get('scoreTotaal')
-    #     # ook voegen de de score per catagorie toe om de gebruiker inzicht te gevenen in zijn prestaties per catagorie
-    #     score1 = request.form.get('score1')
-    #     score2 = request.form.get('score2')
-    #     # etc. (catagorieen nog te bepalen)
-    #     db.execute("INSERT INTO scores (id, username, scoreTotaal, score1, score2, etc., date) VALUES (:sessionid, :username, :score)",
-    #         id = id,
-    #         username = username,
-    #         scoreTotaal = scoreTotaal,
-    #         score1 = score1,
-    #         score2 = score2,
-    #         etc)
-    #     return render_template("leaderboard.html")
-
-    # else:
-        # voordat de quiz begint word er een row binnen de database met daarin een user id aangemaakt voor de gebruiker
-        # daarna word een request gestuurd naar de api voor de vragen die in de quiz moeten komen (50 per categorie, 5 categorieen)
-        # dan worden de vragen om en om gesorteerd zodat de gebruiker ongeveer hetzelfde aantal vragen uit elke catagorie krijgt
+        name = request.form.get("username")
+        db.execute("UPDATE scores SET username = :name WHERE id = :id", name=name, id=session["user_id"])
+        topscores = db.execute("SELECT * FROM scores ORDER BY score")[::-1][:10]
+        return render_template("leaderboard.html", scores=topscores)
     else:
         return render_template("quiz.html")
 
@@ -79,10 +63,10 @@ def check():
     # we returnen dan True/False naar de quiz.html, deze zal de gebruiker om een naam vragen als hij
     # in de top 10 zit zodat we die kunnen laten zien op de leaderboard en anders een pop up geven dat de quiz voorbij is
     userScore = int(request.args.get("score"))
-    db.execute('INSERT INTO scores (score) VALUES (:score)', score=userScore)
-    print(userScore)
+    row = db.execute('INSERT INTO scores (score) VALUES (:score)', score=userScore)
+    session["user_id"] = row
+
     leaderboard= db.execute('SELECT score FROM scores')
-    print(leaderboard)
 
     ranking = 0
     for score in leaderboard:
@@ -103,7 +87,6 @@ def check():
 def leaderboard():
     """Display the current leaderboard and some statistics about the users performance"""
 
-    #session["user_id"] = str(uuid.uuid4())
     topscores = db.execute("SELECT * FROM scores ORDER BY score")[::-1][:10]
 
     return render_template("leaderboard.html", scores=topscores)
@@ -114,4 +97,20 @@ def barchart():
 
     return render_template("barchart.html")
 
+@app.route("/load_questions", methods=["GET"])
+def load_questions():
+    """fetches questions"""
+    a = requests.get("https://opentdb.com/api.php?amount=50&category=9").json()['results'] # general knowledge
+    b = requests.get("https://opentdb.com/api.php?amount=50&category=18").json()['results'] # computer science
+    c = requests.get("https://opentdb.com/api.php?amount=50&category=22").json()['results'] # geopraphy
+    d = requests.get("https://opentdb.com/api.php?amount=50&category=23").json()['results'] # history
+    e = requests.get("https://opentdb.com/api.php?amount=50&category=27").json()['results'] # animals
+    questions = []
+    for i in range(50):
+        questions.append(a[i])
+        questions.append(b[i])
+        questions.append(c[i])
+        questions.append(d[i])
+        questions.append(e[i])
+    return jsonify(questions)
 
