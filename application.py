1b8234c4f5405e429ca99d252c088b2f7b945540
Daan van Baarsen <daanvanbaarsen2008@hotmail.com>
2020-01-21 09:59:36 +0000

    ,

--- application.py
+++ application.py
@@ -1,22 +1,18 @@
 import os
 import uuid
-import requests
-import random
+
 from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 from flask_session import Session
-from tempfile import mkdtemp
 from datetime import datetime
+
+
 
 # Configure application
 app = Flask(__name__)
 
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
-app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
 
 # Ensure responses aren't cached
 @app.after_request
@@ -49,10 +45,29 @@ def quiz():
     """Take quiz"""
 
     if request.method == "POST":
-        name = request.form.get("username")
-        db.execute("UPDATE scores SET username = :name WHERE id = :id", name=name, id=session["user_id"])
-        topscores = db.execute("SELECT * FROM scores ORDER BY score")[::-1][:10]
-        return render_template("leaderboard.html", scores=topscores)
+        print("POST request")
+    #     # nadat de check functie is uitgevoerd worden de username(als de gebruiker in de top 10 zit) en de score gesubmit
+    #     # deze functie voegt deze dan toe aan de database zodat deze op de leaderboard weergegeven kunnen worden
+    #     id = session["user_id"]
+    #     username = request.form.get('username')
+    #     scoreTotaal = request.form.get('scoreTotaal')
+    #     # ook voegen de de score per catagorie toe om de gebruiker inzicht te gevenen in zijn prestaties per catagorie
+    #     score1 = request.form.get('score1')
+    #     score2 = request.form.get('score2')
+    #     # etc. (catagorieen nog te bepalen)
+    #     db.execute("INSERT INTO scores (id, username, scoreTotaal, score1, score2, etc., date) VALUES (:sessionid, :username, :score)",
+    #         id = id,
+    #         username = username,
+    #         scoreTotaal = scoreTotaal,
+    #         score1 = score1,
+    #         score2 = score2,
+    #         etc)
+    #     return render_template("leaderboard.html")
+
+    # else:
+        # voordat de quiz begint word er een row binnen de database met daarin een user id aangemaakt voor de gebruiker
+        # daarna word een request gestuurd naar de api voor de vragen die in de quiz moeten komen (50 per categorie, 5 categorieen)
+        # dan worden de vragen om en om gesorteerd zodat de gebruiker ongeveer hetzelfde aantal vragen uit elke catagorie krijgt
     else:
         return render_template("quiz.html")
 
@@ -64,10 +79,10 @@ def check():
     # we returnen dan True/False naar de quiz.html, deze zal de gebruiker om een naam vragen als hij
     # in de top 10 zit zodat we die kunnen laten zien op de leaderboard en anders een pop up geven dat de quiz voorbij is
     userScore = int(request.args.get("score"))
-    row = db.execute('INSERT INTO scores (score) VALUES (:score)', score=userScore)
-    session["user_id"] = row
-
+    db.execute('INSERT INTO scores (score) VALUES (:score)', score=userScore)
+    print(userScore)
     leaderboard= db.execute('SELECT score FROM scores')
+    print(leaderboard)
 
     ranking = 0
     for score in leaderboard:
@@ -75,12 +90,13 @@ def check():
             ranking+= 1
 
     percentile = ((1-(ranking / len(leaderboard))) * 100)
+
     if ranking < 10:
-        return jsonify(['Congratulations! you are number ' + str(ranking) + ' on the leaderboard.', True])
+        return jsonify('Congratulations! you are number ' + str(ranking) + ' on the leaderboard.')
     elif percentile > 50:
-        return jsonify('Well done you scored better than {0:.2f}% of previous users!'.format(percentile))
+        return jsonify('Well done you scored bettter than {0:.2f}% of previous users!'.format(percentile))
     elif percentile >= 25:
-        return jsonify('You scored better than {0:.2f}% of previous users.'.format(percentile))
+        return jsonify('You scored bettter than {0:.2f}% of previous users.'.format(percentile))
     elif percentile < 25:
         return jsonify('Better luck next time, you scored in the bottom quartile.')
 
@@ -88,6 +104,7 @@ def check():
 def leaderboard():
     """Display the current leaderboard and some statistics about the users performance"""
 
+    #session["user_id"] = str(uuid.uuid4())
     topscores = db.execute("SELECT * FROM scores ORDER BY score")[::-1][:10]
 
     return render_template("leaderboard.html", scores=topscores)
@@ -98,53 +115,4 @@ def barchart():
 
     return render_template("barchart.html")
 
-@app.route("/load_questions", methods=["GET"])
-def load_questions():
-    """fetches questions"""
-    a = requests.get("https://opentdb.com/api.php?amount=50&category=9").json()['results'] # general knowledge
-    b = requests.get("https://opentdb.com/api.php?amount=50&category=18").json()['results'] # computer science
-    c = requests.get("https://opentdb.com/api.php?amount=50&category=22").json()['results'] # geopraphy
-    d = requests.get("https://opentdb.com/api.php?amount=50&category=23").json()['results'] # history
-    e = requests.get("https://opentdb.com/api.php?amount=50&category=27").json()['results'] # animals
 
-    questions = []
-    for i in range(50):
-        questions.append(a[i])
-        questions.append(b[i])
-        questions.append(c[i])
-        questions.append(d[i])
-        questions.append(e[i])
-
-    questions_js = []
-    correct_answers = []
-    for question in questions:
-        answers = question['incorrect_answers']
-        answers.append(question['correct_answer'])
-        random.shuffle(answers)
-
-        questions_js.append({
-            'answers' : answers,
-            'question' : question['question'],
-            'difficulty' : question['difficulty'],
-            'category' : question['category'],
-            'type' : question['type'],
-        })
-
-        correct_answers.append(question['correct_answer'])
-
-    session['correct_answers'] = correct_answers
-
-    return jsonify(questions_js)
-
-
-@app.route("/check_answer")
-def check_answer():
-    answer = request.args.get('answer');
-    correct_answer = session['correct_answers'][0]
-    session['correct_answers'].remove(session['correct_answers'][0])
-    if answer == correct_answer:
-
-        return jsonify(True)
-    else:
-
-        return jsonify(False)
