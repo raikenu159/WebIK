b1fff208f998ac45ec703de1fc53ff0ef8c1d447
raikenu159 <una.g159@gmail.com>
2020-01-28 12:53:27 +0000

    added index.js and <p> tags around header text in layout

---
+++ static/js/index.js
@@ -0,0 +1,9 @@
+function toggle_guide(){
+    guide = document.getElementById("quizGuide");
+    if(guide.style.display === "none"){
+        guide.style.display = "block";
+    }
+    else{
+        guide.style.display = "none";
+    }
+}
