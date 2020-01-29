3ac7d3fcf1e7f44805705f203e2400f757fa4581
Daan van Baarsen <daanvanbaarsen2008@hotmail.com>
2020-01-29 11:45:11 +0000

    overbodige import verwijderd

--- application.py
+++ application.py
@@ -6,7 +6,6 @@ from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
-from datetime import datetime
 
 
 # Configure application
